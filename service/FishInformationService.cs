98634d673f964e370cb586d04d8782a2c44f6b8c
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-10-22 21:00:08 +0700

    service

---
+++ service/FishInformationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bussinessobject;
+using repos;
+
+namespace service
+{
+    public class FishInformationService : IFishIformationService
+    {
+
+        private IFishInformationRepo repo;
+
+        public FishInformationService()
+        {
+            repo = new FishInformationRepo();
+        }
+        public bool addFish(FishInformation fishInformation)
+        {
+            return repo.addFish(fishInformation);
+        }
+
+        public bool delFish(string FishId)
+        {
+            return repo.delFish(FishId);
+        }
+
+        public List<FishInformation> GetFishInformation()
+        {
+            return repo.GetFishInformation();
+        }
+
+        public FishInformation GetFishInformation(string id)
+        {
+            return repo.GetFishInformationByID(id);
+        }
+
+        public bool UpdateFish(string FishId)
+        {
+            return repo.updateFish(FishId);
+        }
+    }
+}
---
+++ service/IFishIformationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bussinessobject;
+
+namespace service
+{
+    public interface IFishIformationService
+    {
+        public List<FishInformation> GetFishInformation();
+
+        public FishInformation GetFishInformation(string id);
+
+        public bool addFish(FishInformation fishInformation);
+
+        public bool delFish(String FishId);
+
+        public bool UpdateFish(String FishId);
+    }
+}
