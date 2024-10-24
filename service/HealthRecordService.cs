a83b76c011b66a6fe69596662d8e618c3a9e7a30
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-10-24 09:49:38 +0700

    code

---
+++ service/HealthRecordService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bussinessobject;
+using Microsoft.IdentityModel.Tokens;
+
+namespace service
+{
+    public class HealthRecordService : IHealthRecordService
+    {
+        private readonly IHealthRecordService healthRecordRepo;
+
+        public HealthRecordService()
+        {
+            healthRecordRepo = new HealthRecordService();
+        }
+        public List<HealthRecord> GetHealthRecords()
+        {
+           
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ service/IUserAccountService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bussinessobject;
+using Microsoft.Identity.Client;
+
+namespace service
+{
+    public interface IUserAccountService
+    {
+        public UserAccount GetUserAccountByEmail(String email);
+
+        public List<UserAccount> GetUserAccounts();
+    }
+}
