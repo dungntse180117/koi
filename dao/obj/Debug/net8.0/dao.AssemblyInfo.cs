e47268080611fe22c200fe59af4d89fbab6a1f0e
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-11-04 10:06:50 +0700

    add

--- dao/obj/Debug/net8.0/dao.AssemblyInfo.cs
+++ dao/obj/Debug/net8.0/dao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b7bbe04340b8e0d3a4ba5b5d697c51c2332ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("dao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
