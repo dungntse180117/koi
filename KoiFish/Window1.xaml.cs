c57a018b66d9e799b229f4c57590bf52fa183f91
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-11-13 22:22:04 +0700

    fix

--- KoiFish/Window1.xaml.cs
+++ KoiFish/Window1.xaml.cs
@@ -10,19 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KoiFish
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class Window1 : Window
     {
-        public Page1()
+        public Window1()
         {
             InitializeComponent();
+        }
+
+        private void btnFish_Click_1(object sender, RoutedEventArgs e)
+        {
+            FishInfoWindow fish = new FishInfoWindow();
+            fish.Show();
+            this.Close();
         }
     }
 }
